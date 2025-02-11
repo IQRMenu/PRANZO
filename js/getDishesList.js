5ba8308f07373ca8b7282a6112924a054e08bdab
IQRMenu <interactivemenuqr@gmail.com>
2025-02-11 20:07:50 -0300

    start

---
+++ js/getDishesList.js
@@ -0,0 +1,30 @@
+export function fetchDishesList() {
+  return fetch('https://script.google.com/macros/s/AKfycbwmM5d62aYv1p_ybR5i8l7lmHmUlxBZ2BrhZGnrYcqHOvbXJobGRp801BPgbdo_o5OV/exec')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Сеть ответила с ошибкой: ' + response.status);
+      }
+      // Преобразуем ответ в JSON
+      return response.json();
+    })
+    .then(data => {
+      return processData(data);
+    })
+    .catch(error => {
+      console.error('Ошибка:', error);
+      return [];
+    });
+}
+
+
+function processData(data) {
+  const keys = data[0];
+  const objectsArray = data.slice(2).map(row => {
+    let obj = {};
+    row.forEach((value, index) => {
+      obj[keys[index]] = value;
+    });
+    return obj;
+  });
+  return objectsArray;
+}
