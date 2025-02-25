93e53d06facc65aa50eec3e182828836b63d4430
IQRMenu <interactivemenuqr@gmail.com>
2025-02-24 22:41:35 -0300

    v5.01 add api google

--- js/getDishesList.js
+++ js/getDishesList.js
@@ -1,19 +1,17 @@
-export function fetchDishesList() {
-  return fetch('https://script.google.com/macros/s/AKfycbwmM5d62aYv1p_ybR5i8l7lmHmUlxBZ2BrhZGnrYcqHOvbXJobGRp801BPgbdo_o5OV/exec')
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Сеть ответила с ошибкой: ' + response.status);
-      }
-      // Преобразуем ответ в JSON
-      return response.json();
-    })
-    .then(data => {
-      return processData(data);
-    })
-    .catch(error => {
-      console.error('Ошибка:', error);
-      return [];
-    });
+export async  function fetchDishesList() {
+  const sheetId = "1oKdM6Bv7wRntfs35yXl840-W1tPcgIt5VBDnsTU-NVs"; // ID твоей таблицы
+  const apiKey = "AIzaSyAoqUP1XTXL7Y5zXyQ9rfEgMy4d30qDC-Q"; // Вставь сюда API-ключ
+  const url = `https://sheets.googleapis.com/v4/spreadsheets/${sheetId}/values/Sheet1?key=${apiKey}`;
+
+  try {
+    let response = await fetch(url);
+    let data = await response.json();
+    
+    return processData(data.values); // Конвертируем в удобный формат
+  } catch (error) {
+    console.error("Ошибка загрузки данных:", error);
+    return [];
+  }
 }
 
 
